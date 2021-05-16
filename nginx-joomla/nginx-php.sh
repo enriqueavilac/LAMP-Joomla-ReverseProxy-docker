cc92a26eb26e1416323f4a0072efaab564766024
Enrique Avila <55473744+enriqueavilac@users.noreply.github.com>
2021-05-16 02:35:33 -0500

    Update nginx-php.sh

--- nginx-joomla/nginx-php.sh
+++ nginx-joomla/nginx-php.sh
@@ -33,8 +33,6 @@ chown -R www-data /usr/share/nginx/html
 echo "server {
     listen 80;
     listen [::]:80;
-    #server_name enriquesite.com;
-    #server_name_in_redirect off;
 
     access_log /var/log/nginx/access.log;
     error_log /var/log/nginx/error.log info;
